466bdd5b6e1d8b0069863a4d46f6e355af6d7536
karthik <karthik18tech@gmail.com>
2025-06-07 10:50:51 +0530

    https server setup

--- socket/server.js
+++ socket/server.js
@@ -2,6 +2,7 @@ const { Server } = require("socket.io");
 const https = require("https");
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 const app = express();
 
